579f2361a38dec226b3563121b36aa5ebbedaf7b
Kyle Ellrott <kellrott@gmail.com>
2019-03-27 00:26:38 -0700

    Switching the javascript interpreter over to a JSON path library

--- evaluate/javascript.go
+++ evaluate/javascript.go
@@ -1,16 +1,15 @@
 package evaluate
 
 import (
-	"encoding/json"
+	//"encoding/json"
 	"fmt"
 	"log"
 	"regexp"
 
-	"gopkg.in/olebedev/go-duktape.v3"
+	"github.com/oliveagle/jsonpath"
 )
 
-var EXP_RE_STRING, _ = regexp.Compile(`(.*)\$\((.*)\)(.*)`)
-var EXP_RE, _ = regexp.Compile(`\$\((.*)\)`)
+var EXP_RE_STRING, _ = regexp.Compile(`$\.(.*)`)
 
 func ExpressionString(expression string, inputs map[string]interface{}) (string, error) {
 
@@ -18,20 +17,15 @@ func ExpressionString(expression string, inputs map[string]interface{}) (string,
 	if matches == nil {
 		return expression, nil
 	}
-	code := matches[2]
-	log.Printf("JS Expression: %s", code)
-	//log.Printf("JS Inputs: %#v", self.Inputs.Normalize())
 
-	ctx := duktape.New()
+	log.Printf("JSON PATH: %s", expression)
 
-	inStr, _ := json.Marshal(inputs)
-
-	if err := ctx.PevalStringNoresult(fmt.Sprintf("var input = %s;", inStr)); err != 0 {
-		return "", fmt.Errorf("Input variable load failed")
+	res, err := jsonpath.JsonPathLookup(inputs, expression)
+	if err != nil {
+		return "", err
 	}
-	if err := ctx.PevalString(code); err != nil {
-		return "", fmt.Errorf("Config string failed: %s", code)
+	if out, ok := res.(string); ok {
+		return out, nil
 	}
-	out := ctx.GetString(-1)
-	return out, nil
+	return "", fmt.Errorf("Need string variable")
 }

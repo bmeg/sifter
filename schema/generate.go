0f13ad5b078aa7b1672ab440c158c9ef78776543
Kyle Ellrott <kellrott@gmail.com>
2019-05-13 11:22:02 -0700

    Fixing object edge creation code

--- schema/generate.go
+++ schema/generate.go
@@ -2,7 +2,7 @@
 package schema
 
 import (
-  //"log"
+  "log"
   "fmt"
   "github.com/bmeg/grip/gripql"
   "github.com/bmeg/grip/protoutil"
@@ -49,8 +49,8 @@ func (s Schema) Validate(data map[string]interface{}) (map[string]interface{}, e
 
 func (s Schema) Generate(data map[string]interface{}) ([]GraphElement, error) {
   out := make([]GraphElement, 0, 1+len(s.Links)*2)
+  outData := map[string]interface{}{}
 
-  ds := protoutil.AsStruct(data)
   gid := ""
   for k, v := range s.Props {
     if v.Element.SystemAlias == "node_id" {
@@ -59,19 +59,40 @@ func (s Schema) Generate(data map[string]interface{}) ([]GraphElement, error) {
           gid = ks
         }
       }
+    } else {
+      if x, ok := data[k]; ok {
+        outData[k] = x
+      }
     }
   }
+  ds := protoutil.AsStruct(outData)
   v := gripql.Vertex{Gid: gid, Label: s.Title, Data:ds}
 
   out = append(out, GraphElement{Vertex:&v})
 
   for _, l := range s.Links {
-    //TODO: figure out dest variables
-    e := gripql.Edge{From:gid, Label:l.Label}
-    out = append(out, GraphElement{Edge:&e})
-    if l.Backref != "" {
-      e := gripql.Edge{To:gid, Label:l.Backref}
+    dst := []string{}
+    log.Printf("%s", s.Props[l.Name].Element.Type.Type)
+    if s.Props[l.Name].Element.Type.Type == "string" {
+      if x, ok := data[l.Name].(string); ok {
+        dst = []string{ x }
+      } else {
+        log.Printf("Wrong: %s", data[l.Name])
+      }
+    } else if s.Props[l.Name].Element.Type.Type == "array" {
+      if x, ok := data[l.Name].([]string); ok {
+        dst = x
+      } else {
+        log.Printf("Wrong: %s %s %s", data[l.Name], l.Name, data)
+      }
+    }
+    for _, d := range dst {
+      e := gripql.Edge{From:gid, To: d, Label:l.Label}
       out = append(out, GraphElement{Edge:&e})
+      if l.Backref != "" {
+        e := gripql.Edge{To:gid, From: d, Label:l.Backref}
+        out = append(out, GraphElement{Edge:&e})
+      }
     }
   }
   return out, nil

25c94da43e957e8b006e0585d5ff4410ac4f9882
Kyle Ellrott <kellrott@gmail.com>
2020-05-02 22:31:36 -0700

    Updating templating to use `inputs` rather then `input` which better matches the structure of the config document

--- evaluate/template.go
+++ evaluate/template.go
@@ -7,7 +7,7 @@ import (
 )
 
 func ExpressionString(expression string, inputs map[string]interface{}, row map[string]interface{}) (string, error) {
-	d := map[string]interface{}{"input":inputs}
+	d := map[string]interface{}{"inputs":inputs}
 	if row != nil {
 		d["row"] = row
 	}

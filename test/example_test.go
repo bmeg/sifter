df131f9fe604e77386a747b81339c3072e119e25
Kyle Ellrott <kellrott@gmail.com>
2021-02-10 18:49:46 -0800

    Fixing lint issues

--- test/example_test.go
+++ test/example_test.go
@@ -1,7 +1,7 @@
 package test
 
 import (
-  "fmt"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"testing"
@@ -10,25 +10,25 @@ import (
 	"github.com/bmeg/sifter/loader"
 	"github.com/bmeg/sifter/manager"
 
-  "path/filepath"
+	"path/filepath"
 
-  "github.com/ghodss/yaml"
+	"github.com/ghodss/yaml"
 )
 
 type ExampleConfig struct {
-  Playbook string         `json:"playbook"`
-  Inputs map[string]string `json:"inputs"`
-  Outputs []string        `json:"outputs"`
+	Playbook string            `json:"playbook"`
+	Inputs   map[string]string `json:"inputs"`
+	Outputs  []string          `json:"outputs"`
 }
 
 func runPlaybook(playbook string, inputs map[string]interface{}, outdir string) error {
-  workDir := "./"
-  os.Mkdir(outdir, 0700)
+	workDir := "./"
+	os.Mkdir(outdir, 0700)
 	driver := "dir://" + outdir
 	ld, err := loader.NewLoader(driver)
 	if err != nil {
-    return err
-  }
+		return err
+	}
 	defer ld.Close()
 
 	dir, err := ioutil.TempDir(workDir, "sifterwork_")
@@ -36,7 +36,7 @@ func runPlaybook(playbook string, inputs map[string]interface{}, outdir string) 
 
 	man, err := manager.Init(manager.Config{Loader: ld, WorkDir: workDir, DataStore: nil})
 	if err != nil {
-    return err
+		return err
 	}
 	defer man.Close()
 	man.AllowLocalFiles = true
@@ -46,46 +46,46 @@ func runPlaybook(playbook string, inputs map[string]interface{}, outdir string) 
 		return err
 	}
 	os.RemoveAll(dir)
-  return nil
+	return nil
 }
 
 func fileExists(filename string) bool {
-    info, err := os.Stat(filename)
-    if os.IsNotExist(err) {
-        return false
-    }
-    return !info.IsDir()
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
 }
 
 func TestExamples(t *testing.T) {
-  tests, err := filepath.Glob("test-*.yaml")
-  if err != nil {
-    t.Error(err)
-  }
-  for _, tPath := range tests {
-    raw, err := ioutil.ReadFile(tPath)
-  	if err != nil {
-  		t.Error(fmt.Errorf("failed to read config %s", tPath))
-  	}
-  	conf := ExampleConfig{}
-  	if err := yaml.Unmarshal(raw, &conf); err != nil {
-      t.Error(fmt.Errorf("failed to read config %s", tPath))
-  	}
-    inputs := map[string]interface{}{}
-    for k, v := range conf.Inputs {
-      inputs[k] = v
-    }
-    fmt.Printf("%s\n", conf)
-    outDir, err := ioutil.TempDir("./", "testout_")
-    runPlaybook(conf.Playbook, inputs, outDir)
+	tests, err := filepath.Glob("test-*.yaml")
+	if err != nil {
+		t.Error(err)
+	}
+	for _, tPath := range tests {
+		raw, err := ioutil.ReadFile(tPath)
+		if err != nil {
+			t.Error(fmt.Errorf("failed to read config %s", tPath))
+		}
+		conf := ExampleConfig{}
+		if err := yaml.Unmarshal(raw, &conf); err != nil {
+			t.Error(fmt.Errorf("failed to read config %s", tPath))
+		}
+		inputs := map[string]interface{}{}
+		for k, v := range conf.Inputs {
+			inputs[k] = v
+		}
+		fmt.Printf("%s\n", conf)
+		outDir, err := ioutil.TempDir("./", "testout_")
+		runPlaybook(conf.Playbook, inputs, outDir)
 
-    for _, out := range conf.Outputs {
-      base := filepath.Base(out)
-      dst := filepath.Join(outDir, base)
-      if !fileExists(dst) {
-        t.Errorf("Output %s not produced", base)
-      }
-    }
-    os.RemoveAll(outDir)
-  }
+		for _, out := range conf.Outputs {
+			base := filepath.Base(out)
+			dst := filepath.Join(outDir, base)
+			if !fileExists(dst) {
+				t.Errorf("Output %s not produced", base)
+			}
+		}
+		os.RemoveAll(outDir)
+	}
 }

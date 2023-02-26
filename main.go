e861c811d6835754d1ab46115cd65834abf066a7
Kyle Ellrott <kellrott@gmail.com>
2023-02-26 13:54:53 -0800

    Fixing logging issues introduced by linking to github.com/vitessio/vitess

--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/bmeg/sifter/cmd"
 )
 
 func main() {
-	//flag.Parse()
+	flag.Set("v", "2")
+	flag.Parse()
+	log.SetFlags(0)
+	log.SetOutput(os.Stderr)
+
 	if err := cmd.RootCmd.Execute(); err != nil {
 		fmt.Println("Error:", err.Error())
 		os.Exit(1)
